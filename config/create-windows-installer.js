b07cbf123dfca647df95dc35fd0334e5e8a503ea
Tobias Oettl <toettl@united-internet.de>
2019-03-08 13:53:53 +0100

    Working on release.

---
+++ config/create-windows-installer.js
@@ -0,0 +1,26 @@
+const createWindowsInstaller = require('electron-winstaller')
+  .createWindowsInstaller;
+const path = require('path');
+
+getInstaller()
+  .then(createWindowsInstaller)
+  .catch(error => {
+    console.error(error.message || error);
+    process.exit(1);
+  });
+
+function getInstallerConfig() {
+  console.log('Creating windows installer...');
+  const rootPath = path.join('./');
+  const outPath = path.join(rootPath, 'release-builds');
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'violin-win32-ia32/'),
+    authors: 'Tobias "CodeF0x" Oettl',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    exe: 'voilin.exe',
+    setupExe: 'voilin-setup.exe',
+    setupIcon: path.join(rootPath, 'src', 'img', 'icon', 'icon.ico')
+  });
+}
