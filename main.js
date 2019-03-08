21b358dc1aaf4c4acb7d7c9d50adba46caae8a11
Tobias Oettl <toettl@united-internet.de>
2019-03-08 15:10:55 +0100

    Handled installation on Windows.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// this is for handling the installation of the app on Windows
+const handleSetupEvents = require('./config/setup-events');
+if (handleSetupEvents.handleSquirrelEvent()) {
+  return;
+}
+
 const { app, BrowserWindow } = require('electron');
 const { dialog } = require('electron');
 const { ipcMain } = require('electron');
