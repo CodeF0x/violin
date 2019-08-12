5c4def89d74033bdfe5368d00ff5676498c7fbc8
CodeF0x <contact@codef0x.dev>
2019-08-12 21:28:39 +0200

    Fixed buggy media keys.

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,7 @@ class Main {
       switch (key) {
         case 'MediaPlayPause':
           self._Player.playPause(self, self._UI);
-          UI.updateUI(self, self._Player);
+          self._UI.togglePlayButton(self, self._Player);
           break;
         case 'MediaNextTrack':
           self._Player.next(self._UI, self);
