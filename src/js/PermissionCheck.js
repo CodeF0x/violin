1c4b0b359ca16ecd3c943c74054cff70f89acfe3
CodeF0x <contact@codef0x.dev>
2019-08-27 09:16:17 +0200

    Asking for permission.

--- src/js/PermissionCheck.js
+++ src/js/PermissionCheck.js
@@ -3,12 +3,18 @@ class PermissionCheck {
     if (!process.platform === 'darwin') {
       return;
     }
+
+    const { app, systemPreferences } = require('electron').remote;
+
+    if (systemPreferences.isTrustedAccessibilityClient(true)) {
+      return;
+    }
+
     const not = new Notification('Enable keyboard shortcuts', {
       body:
         'Please add Violin as a trusted client, then click on this message or restart Violin.'
     });
     not.onclick = () => {
-      const app = require('electron').remote.app;
       app.relaunch();
       app.quit();
     };
